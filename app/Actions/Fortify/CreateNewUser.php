edea95ee07e60609308acd56cb3c9c2df2d915c9
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-02-16 08:46:52 +0800

    Some changes

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -28,6 +28,7 @@ class CreateNewUser implements CreatesNewUsers
 
         return User::create([
             'name' => $input['name'],
+            'role' => 'user', // default => user (others => [ admin, accounting ])
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
